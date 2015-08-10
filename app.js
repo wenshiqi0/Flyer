e66cc7c77c0b6990d0c589a2389efa3e3169f2df
Winsky <wenshiqi0@hotmail.com>
2015-08-10 18:56:07 +0800

    Flyer

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
  * Created by Winsky on 15/8/7.
  */
 'use strict'
+let fs = require('fs');
 let app = require('./lib/index');
-let Flyer = require('./lib/flyer');
+let Flyer = require('./lib/Flyer');
+let Promise = require('bluebird');
+let Route =require('./midware/flyer-route');
 
-let f = new Flyer();
+app.launch(new Route('/',function*(){
+    let html = yield Promise.fromNode(function(cb){
+        fs.readFile('index.html',cb)
+    });
+    this.res.writeHeader(200,{'Content-Type':'text/html'});
+    this.res.end(html);
+}));
 
-app.controller(f);
+app.launch(new Route('/new',function*(){
+    let html = yield Promise.fromNode(function(cb){
+        fs.readFile('new.html',cb)
+    });
+    this.res.writeHeader(200,{'Content-Type':'text/html'});
+    this.res.end(html);
+}));
 
 app.listen(3000,'localhost');
 
---
+++ midware/flayer-logger.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2015/8/10.
+ */
+'use strict'
+let Flyer = require('flyer');
+
+class Flyer_Logger extends Flyer{
+    constructor(){
+        super();
+    }
+
+    *do(){
+
+    }
+}
+
+module .exports = Flyer_Logger;
